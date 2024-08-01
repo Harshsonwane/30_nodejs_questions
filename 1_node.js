044416e492e029dcd16d3d270c11eac1981a6892
harshsonwane <harshsonwane05@gmail.com>
2024-08-02 00:06:09 +0530

    code changes

--- 1_node.js
+++ 1_node.js
@@ -4,10 +4,18 @@
 const express = require('express')
 const app = express();
 
+// sigle parameter
 app.get('/hello/:username',(req,res)=>{
     const username = req.params.username;
     res.send(`Hello ${username}`);
 });
+
+// multiparameter
+
+app.get('/info/:username/:age',(req,res)=>{
+    const { username , age } = req.params;
+    res.send(`HEllo buddy your username is ${username} and your age is ${age}`) 
+})
 
 app.listen(5000,()=>{
     console.log("i am inside your pc")
